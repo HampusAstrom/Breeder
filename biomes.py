78def30419233cfa52b827b4fd5aea05727d6e19
hanna <hmt.autio@gmail.com>
2018-06-30 21:33:35 +0200

    Modified the evaluation algorithms for biome fitness slightly

--- biomes.py
+++ biomes.py
@@ -1,40 +1,41 @@
 # import Creature from creature.py as creatures
 
 forest_ranges = {
-    'cold_resist' : [-2, 50],
-    'heat_resist' : [-2, 50],
-    'energy_need' : [0, 50],
-    'tree_climber' : [-4, 50]
+    'cold_resist' : ['lower', -2],
+    'heat_resist' : ['lower', -2],
+    'energy_need' : ['upper', 30],
+    'tree_climber' : ['lower', -4]
 }
 
 desert_ranges = {
-    'cold_resist' : [-2, 50],
-    'heat_resist' : [3, 50],
-    'energy_need' : [0, 25],
-    'foraging' : [2, 50]
+    'cold_resist' : ['lower', -2],
+    'heat_resist' : ['lower', 3],
+    'energy_need' : ['upper', 25],
+    'foraging' : ['lower', 2]
 }
 
 tundra_ranges = {
-    'cold_resist' : [3, 50],
-    'energy_need' : [0, 25],
-    'foraging' : [2, 50]
+    'cold_resist' : ['lower', 3],
+    'energy_need' : ['upper', 25],
+    'foraging' : ['lower', 2]
 }
 
 savannah_ranges = {
-    'heat_resist' : [3, 50],
-    'energy_need' : [0, 50]
+    'heat_resist' : ['lower', 3],
+    'energy_need' : ['upper', 30]
 }
 
 mountain_ranges = {
-    'cold_resist' : [2, 50],
-    'energy_need' : [0, 25],
-    'foraging' : [2, 50]
+    'cold_resist' : ['lower', 2],
+    'energy_need' : ['upper', 25],
+    'foraging' : ['lower', 2]
 }
 
 domestic_ranges = {
-    'tame' : [5, 100],
-    'intelligent' : [2, 8],
-    'social' : [5, 20]
+    'tame' : ['lower', 5],
+    'intelligent' : ['lower', 2],
+    'intelligent' : ['upper', 8],
+    'social' : ['lower', 5]
 }
 
 der_ab_ranges = {
@@ -66,11 +67,18 @@ class Biome:
     def check_fitness(self, creature_abilities):
         fitness = 0
         inc = 10/len(self._ab_ranges)
-        for ab, span in self._ab_ranges.items():
-            if span[0] < creature_abilities[ab] and span[1] > creature_abilities[ab]:
-                fitness += inc
+        for ab, limit in self._ab_ranges.items():
+            if limit[0] == 'lower':
+                if limit[1] <= creature_abilities[ab]:
+                    fitness += inc
+                else:
+                    fitness -= inc
             else:
-                fitness -= inc
+                if limit[1] >= creature_abilities[ab]:
+                    fitness += inc
+                else:
+                    fitness -= inc
+
         return fitness
 
     def get_name(self):
